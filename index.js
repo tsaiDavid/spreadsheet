f71e610b1782c0b0ed10a00f13d7567a08804409
David Tsai <david@whataniceguy.com>
2018-06-19 08:40:04 -0700

    Add header row

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   const rootNode = document.getElementById('app')
-  console.log(rootNode)
+  const columns = ['A', 'B', 'C', 'D', 'E'] 
+
+  let table = document.createElement('div')
+  table.className = 'container'
+ 
+  // Make Header
+  columns.forEach(col => {
+    const colNode = document.createElement('div')
+    colNode.appendChild(document.createTextNode(col))
+    table.appendChild(colNode)
+  })
+
+  // Make Rows
+
+  rootNode.appendChild(table)
 })
