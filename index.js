9d0b2ec31b9610fe6b4c30917846a1e20293162f
David Tsai <david@whataniceguy.com>
2018-08-02 07:10:23 -0700

    Adding last changes

--- index.js
+++ index.js
@@ -30,18 +30,35 @@ document.addEventListener('DOMContentLoaded', () => {
       rowParent.setAttribute('id', row)
 
       columns.forEach(col => {
-        const rowNode = document.createElement('div')
+        const rowNode = document.createElement('input')
         rowNode.setAttribute('class', 'cell')
         rowNode.setAttribute('id', `${col}${row}`)
-        rowNode.addEventListener('click', () => {
-          const inputText = window.prompt('enter field', rowNode.innerText)
-          rowNode.innerText = inputText
-        })
+        // rowNode.addEventListener('click', () => {
+        //   const inputText = window.prompt('enter field', rowNode.innerText)
+        //   rowNode.innerText = inputText
+        // })
+        rowNode.addEventListener('change', (e) => {
+          if (e.target.value[0] === "=") {
+            const tmpArr = e.target.value.split('').slice(1)
+            const col = tmpArr[0] 
+            const row = tmpArr[1]
+            if (findCell(col, row)) {
+              rowNode.value = findCell(col, row)
+            }
+          }
+        }) 
         rowParent.appendChild(rowNode)
       })
 
       table.appendChild(rowParent)
     })
+  }
+
+  const findCell = (col, row) => {
+    const node = document.getElementById(`${col}${row}`)
+    if (node) {
+      return node.value
+    }
   }
 
   makeRows(columns, rows)
